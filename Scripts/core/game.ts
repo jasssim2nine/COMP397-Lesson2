fe1f851925ca54cf5af7a4f17ad12ca89b265a1c
Jasim Khan <jasim.khan89@gmail.com>
2018-07-13 19:16:45 -0400

    initial commit

---
+++ Scripts/core/game.ts
@@ -0,0 +1,46 @@
+// IIFE - Immediately Invoked Function Expression
+
+(function(){
+     
+
+    //Game variables
+
+    let canvas = document.getElementById("canvas");
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+
+    function Init():void{
+        console.log("Initialization Started");
+            Start();
+    }
+
+    function Start():void{
+        console.log("Starting Application....");
+
+        stage = new createjs.Stage(canvas);
+        createjs.Ticker.framerate = 60; //60 FPS
+        createjs.Ticker.on("tick", Update);
+        Main();
+        
+    }
+    
+    function Update():void
+    {
+        helloLabel.rotation -= 5;
+        stage.update();
+    }
+    
+    
+    function Main():void{
+        console.log("Game Started...");
+
+        helloLabel = new createjs.Text("Hello World!","40px consolas","#000000");
+        helloLabel.x = 100;
+        helloLabel.y = 100;
+        stage.addChild(helloLabel);
+
+    }
+
+    window.onload = Init;
+
+})();
